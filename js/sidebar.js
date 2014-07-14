e9eb428d4e5a74c580b7bb7868daa7a5da6acbb2
Teddy K.S <thexeons@gmail.com>
2014-07-14 13:47:56 +0700

    fixed chrome sidebar glow bug

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,15 +7,15 @@ var initGlow = function(){
 		linkCount = sidebar.children.length
 		
 	this.onLinkHover = function(e){
-		TweenMax.to(glow,1,{className: 'glow-' + e.target.getAttribute('data-color')});
+		TweenMax.to(glow,1,{className:'glow-' + e.target.getAttribute('data-color')});
 	}
 	this.onLinkExit = function(e){
-		TweenMax.to(glow,2,{className: 'glow-' + main.getAttribute('data-color')});
+		TweenMax.to(glow,2,{className:'glow-' + main.getAttribute('data-color')});
 	}
 
 	for (var i=0; i < linkCount; i++) {
 		var link = sidebar.children[i];
-		link.addEventListener('mouseenter', this.onLinkHover, true);
+		link.addEventListener('mousemove', this.onLinkHover, false);
 		link.addEventListener('mouseout', this.onLinkExit, false);
 	}
 }

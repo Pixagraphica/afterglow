14f774f2df2e5671047124ba64a9d3c1da60258e
NathanChrs <jonathan.christopher@outlook.com>
2014-08-09 12:19:27 +0700

    added paskibra info

--- info/paskibra.php
+++ info/paskibra.php
@@ -1,20 +1,173 @@
 
 			
 			<div class="content-item content-main" >
-				<h2 class="neon-<?php echo $colors["paskibra"]; ?>">Peraturan</h2>
+				<h2 class="neon-<?php echo $colors["paskibra"]; ?>">Ketentuan Umum</h2>
+				
+				<strong>Peserta</strong><br>
+				<ul> 
+					<li>Regu peserta adalah pelajar SMP/SMA, terdaftar di panitia dan telah menyelesaikan kegiatan admistrasi.
+						<ol>
+							<li>Membayar kontribusi pendaftaran sebesar Rp 250.000,-/regu.</li>
+							<li>Mengisi formulir pendaftaran yang telah disediakan panitia</li>
+							<li>Melampirkan surat tugas yang dibubuhi stempel dan tanda tangan kepala sekolah.</li>
+						</ol>
+					</li>
+					<li>Setiap regu hanya terdiri dari pelajar SMP atau SMA saja.</li>
+					<li>Regu peserta terdiri dari 15 orang anggota, 1 danton, dan 2 orang cadangan (tidak wajib).</li>
+					<li>Regu peserta terdiri dari putra dan putri dengan perbandingan bebas.</li>
+					<li>Regu peserta yang melakukan kecurangan dalam keanggotaan regu akan mendapat sanksi diskualifikasi.</li>
+					<li>Pendaftaran paling lambat Jumat, 24 Oktober 2014.</li>
+				</ul>
+				 
+				<strong>Waktu dan Tempat</strong><br>
 				<p>
-					<strong>Proposal Terpisah</strong>
+					<strong>Technical Meeting</strong><br>
+					Tanggal: Jumat,24 Oktober 2014<br>
+					Tempat: SMAK Penabur Gading Serpong<br>
 				</p>
+				<p>
+					<strong>Lomba</strong><br>
+					Tanggal: Kamis, 6 Novermber 2014<br>
+					Tempat: Selasar SMAK Penabur Gading Serpong<br> 
+				</p>
+				
+				<strong>Kewajiban Umum Peserta</strong><br>
+				<ul>
+					<li>Peserta dilarang meninggalkan lokasi perlombaan selama lomba berlangsung.</li>
+					<li>Menjaga kebersihan, keamanan, dan ketertiban dilingkungan/lokasi lomba dan atau pada saat menuju/meninggalkan lokasi lomba.</li>
+					<li>Menjaga persatuan dan kesatuan antar peserta dan pelajar.</li>
+					<li>Melaksanakan dalam kegiatan nyata tujuan-tujuan yang tersirat dalam peraturan baris-berbaris.</li>
+					<li>Mematuhi aturan panitia.</li>
+					<li>Peserta wajib menjaga dengan baik atribut dan perlengkapan milik panitia yang dibawanya.</li>
+					<li>Seluruh peserta dilarang untuk melakukan kecurangan dan usaha untuk mengganggu</li>
+					<li>Pihak peserta yang akan mengabadikan kegiatan (foto & video) mohon untuk tidak memasuki arena lomba.</li>
+					<li>Penghitungan waktu yang berlaku mutlak adalah penghitungan waktu dari panitia.</li>
+					<li>Segala sesuatu yang berkaitan dengan peserta lomba di luar pelaksanaan lomba bukan merupakan tanggung jawab panitia.</li>
+					<li>Wilayah lomba merupakan wilayah bebas asap rokok.</li>
+				</ul>
+
+				<strong>Supporter</strong><br>
+				<ul>
+					<li>Regu peserta diperbolehkan membawa supporter sebanyak-banyaknya selama tidak mengganggu ketertiban lalu lintas disekitar lokasi perlombaan.</li>
+					<li>Supporter wajib memberikan dukungan yang sportif dengan menjaga keamanan, ketertiban, dan kesatuan siswa.</li>
+					<li>Supporter diwajibkan menjaga kebersihan lingkungan lokasi lomba.</li>
+					<li>Supporter dilarang keras mengganggu konsentrasi peserta.</li>
+					<li>Supporter dilarang membawa senjata tajam/api, narkotika, dan obat-obatan terlarang. 
+					<li>Panitia tidak bertanggung jawab atas kehilangan atau kerusakan barang-barang yangdibawa peserta. 
+				</ul>
+
+				<strong>Peringatan Panitia</strong><br>
+				<ul>
+					<li>Untuk kelancaran kegiatan ini dimohon kerjasama yang baik dari peserta lomba.</li>
+					<li>Panitia tidak bertanggung jawab atas kehilangan atau kerusakan barang-barang yang dibawa peserta dan pembina.</li>
+					<li>Mengingat panjangnya kegiatan ini, seluruh peserta diharapkan agar menjaga kondisi fisik dan obat-obatan pribadi dibawa.</li>
+					<li>Panitia tidak menanggung konsumsi para peserta lomba.</li>
+					<li>Selama pelaksanakan lomba dimungkinkan terdapat gangguan-gangguan yang berasal dari pengeras suara lapangan, untuk itu kami mohon antisipasi peserta terhadap keadaan tersebut.</li>
+				</ul>
+					
+				<strong>Sanksi</strong><br>
+				<ul>
+					<li>Sanksi di tempat berupa teguran akan diberikan kepada peserta yang membunyikan HP, membuang sampah sembarangan, membuat kegaduhan, memberikan tepukan dan sorakan yang tidak wajar, dsb.</li>
+					<li>Sanksi pengurangan nilai sebesar 100 point diberikan kepada regu peserta yang mengganggu peserta lain dalam melaksanakan perlombaan (misal: memberikan aba-aba pelaksanaan, memanggil-manggil nama peserta yang sedang melaksanakan lomba).</li>
+					<li>Sanksi denda sebesar Rp 100.000, - dan pengurangan nilai sebanyak 100 poin diberikan kepada regu peserta yang pembina/official-nya merokok selama pelaksanaan lomba.</li>
+					<li>Sanksi pengguguran hak juara diberikan kepada regu peserta yang mengganti anggota pasukan pada saat lomba baris-berbaris.</li>
+					<li>Sanksi diskualifikasi diberikan kepada: 
+						<ol>
+							<li>Regu yang anggotanya memasuki ruang juri dan rekapitulasi nilai dengan alasan apapun juga.</li>
+							<li>Sekolah yang siswa-nya merokok selama pelaksanaan lomba.</li>
+							<li>Regu peserta yang melakukan kecurangan (menggunakan kertas contekan, bantuan hafalan dari official/pembina, menggunakan headset wireless, dll).</li>
+						</ol>
+					</li>
+				</ul>
+				
+				<strong>Hadiah</strong><br>
+				<p>
+					Juara 1: Rp 2.000.000,- + Trofi <br>
+					Juara 2: Rp 1.750.000,- + Trofi <br>
+					Juara 3: Rp 1.250.000,- + Trofi <br>
+				</p>
+				
+				<h2 class="neon-<?php echo $colors["paskibra"]; ?>">Ketentuan Khusus</h2>
+				
+				<ol>
+					<li>Ukuran lapangan yang digunakan adalah 11m x 20m berbentuk persegi panjang.</li>
+					<li>Jumlah peserta/regu lomba baris berbaris adalah 18 siswa dengan perincian: 
+						<ul>
+							<li>Danton barisan :1 siswa</li>
+							<li>Anggota barisan: 15 siswa</li> 
+							<li>Anggota cadangan: 2 siswa</li>
+						</ul>
+					</li>
+					<li>Pakaian regu peserta adalah pakaian lomba masing-masing regu peserta.</li>
+					<li>Regu peserta wajib menggunakan tutup kepala/topi sekolah (kecuali yang memakai jilbab).</li>
+					<li>Dilarang menggunakan Atribut Paskibra (Evolet dan Lembaga Kenegaraan).</li>
+					<li>Waktu dihitung saat Ketua Dewan Juri memberi aba-aba perintah “LAKSANAKAN” dan dihentikan pada saat aba-aba perintah “BUBARKAN”.</li>
+					<li>Danton Regu diberikan nomor urut tampil yang harus dipasang di pakaian dada sebelah kiri.</li>
+					<li>Waktu yang diberikan untuk tampil adalah 15 menit.</li>
+					<li>Ketika Regu 1 tampil, Regu 2 sudah berbaris siap mengikuti lomba di luar arena lomba, dan seterusnya. Regu dengan giliran berikutnya sudah stand by ketika regu dengan giliran sebelumnya sedang tampil.</li>
+					<li>Regu peserta memasuki lapangan lomba dengan cara langkah tegap maju dan berbaris 3 banjar di tengah-tengah lapangan lomba menghadap penuh kepada Ketua Dewan Juri.</li>
+					<li>Danton Regu tampil kurang lebih 3 langkah di depan tengah-tengah regu, memberi aba-aba penghormatan kepada Ketua Dewan Juri.</li>
+					<li>Danton Regu menghadap penuh kepada Ketua Dewan Juri dan melaksanakan laporan awal: ”LAPOR, PESERTA DENGAN NOMOR ... DARI SEKOLAH ... SIAP MELAKSANAKAN LOMBA”. Kemudian Ketua Dewan Juri menjawab ”LAKSANAKAN”. Setelah itu Danton Regu menjawab ”SIAP LAKSANAKAN” dan menuju tempat khusus yang telah disediakan panitia.</li>
+					<li>Danton Regu hanya tampil pada saat Formasi/Varian.</li>
+					<li>Apabila waktu tampil habis atau selesai melaksanakan rangkaian gerakan lomba, maka Danton Regu dan barisannya kembali menghadap penuh Ketua Dewan Juri untuk melaksanakan laporan akhir: ”LOMBA TELAH DILAKSANAKAN, LAPORAN SELESAI”. Kemudian Ketua Dewan Juri menjawab ”BUBARKAN” dan dibalas oleh Danton Regu ”SIAP BUBARKAN”, lalu memberikan aba-aba penghormatan kepada Ketua Dewan Juri.</li>
+					<li>Regu peserta keluar dari lapangan lomba dengan cara Langkah Biasa.</li>
+					<li>Bunyi peluit 1 (satu) kali: Pertanda waktu dimulai. Bunyi peluit 2 (dua) kali dan bendera kuning dilambaikan: 2 menit terakhir. Bunyi peluit 3 (tiga) kali: waktu sudah habis.</li>
+					<li>Danton Regu berada di dalam garis batas lapangan dan tidak boleh berpindah tempat (tempat Danton Regu telah ditentukan).</li>
+					<li>Melebihi garis batas lapangan dianggap tidak melaksanakan gerakan tersebut dan dikenai pengurangan nilai.</li>
+					<li>Garis batas lapangan menggunakan prinsip ”GARIS TEMBOK”. Artinya apabila ada peserta yang menginjak garis batas dan/atau melayang di atas garis batas dianggap telah melebihi garis batas.</li>
+					<li>Melebihi batas waktu, kegiatan dihentikan karena gerakan selanjutnya tidak akan dinilai.</li>
+					<li>Daftar urutan gerakan/aba-aba sebagaimana dalam petunjuk penilaian.</li>
+					<li>Peserta disarankan tidak mengulang-ulang aba-aba yang sama karena gerakan yang dinilai adalah gerakan pertama.</li>
+					<li>Apabila ada aba-aba yang terlewati maka aba-aba tersebut dianggap tidak dilaksanakan.</li>
+					<li>Panggilan terhadap peserta dilaksanakan sebanyak 3 (tiga) kali dengan interval 2 menit. Apabila peserta belum hadir maka mendapatkan pengurangan/penalti dari nilai akhir dan diberi kesempatan melaksanakan pada urutan terakhir.</li>
+					<li>Catatan:
+						<ul>
+							<li>Urutan gerakan sesuai dengan urutan unsur dan aspek seperti tersebut dalam unsur dan aspek yang dinilai.</li>
+							<li>Bila pada 2 unsur / aspek yang berurutan tidak mungkin dilakukan, gerakan yang berurutan dapat disisipi gerakan lain yang sesuai dengan peraturan baris berbaris, maksimal 2 aba-aba sisipan.</li>
+							<li>Tidak diperbolehkan menggunakan aba-aba rangkap lebih dari 2, kecuali pada saat gerakan variasi.</li>
+						</ul
+					</li>
+				</ol>
+
+				<strong>Pengurangan Nilai</strong><br>
+				<ol>
+					<li>Melebihi waktu tampil -25/1 (minus dua puluh lima per satu) menit dari nilai akhir.</li>
+					<li>Melewati garis lapangan lomba -10/1 (minus sepuluh per satu) saf dari nilai akhir.</li>
+					<li>Anggota regu tidak memakai topi -10/1 (minus sepuluh per satu) orang dari nilai akhir.</li>
+					<li>Anggota regu kurang dari 16 orang -50/1 (minus lima puluh per satu) orang dari nilai akhir.</li> 
+					<li>Anggota sakit/pingsan di lapangan -50/1 (minus lima puluh per satu) orang dari nilai akhir.</li>
+					<li>Danton sakit/pingsan di lapangan -100 (minus seratus) dari nilai akhir.</li>
+					<li>Terlambat memenuhi panggilan sebanyak 3 (tiga) kali -100 (minus seratus) dari nilai akhir.</li>
+					<li>Memanipulasi data anggota regu atau melakukan kecurangan dalam keanggotaan, mendapat sanksi diskualifikasi.</li>
+				</ol>
+
+				<strong>Penentuan Juara</strong><br>
+				<ol>
+					<li>Juara ditentukan berdasarkan jumlah nilai tertinggi dari Dewan Juri dan telah dikurangi pinalti.</li>
+					<li>Apabila ada 2 regu peserta atau lebih memiliki jumlah nilai yang sama, maka penentuan peringkat ditentukan dari waktu yang paling mendekati 15 menit.</li>
+				</ol>
+
+				<strong>Proposal</strong><br>
+				<p>Form penilaian dan form pendaftaran tercantum dalam proposal lengkap: <a href="proposal-paskibra.pdf">DOWNLOAD</a> </p>
+				
 			</div>
 				
 			<div class="content-item">
 				<h3 class="neon-<?php echo $colors["paskibra"]; ?>">Contact Person</h3>
-				0878 8910 1280<br>
+				<strong>Giovanna:</strong> 0878 8910 1280<br>
+				<strong>Debora:</strong> 0817 4841 569<br>
 			</div>
 			
-			<!--<div class="content-item">
+			<div class="content-item">
+				<h3 class="neon-<?php echo $colors["paskibra"]; ?>">Biaya Pendaftaran</h3>
+				<strong>Rp 250.000,- per regu</strong>
+			</div>
+			
+			<div class="content-item">
 				<h3 class="neon-<?php echo $colors["paskibra"]; ?>">Jadwal Acara</h3>
+				<p><strong>Batas Akhir Pendaftaran:</strong> 24 Oktober 2014 </p>
+				<p><strong>Technical Meeting:</strong> 24 Oktober 2014 </p>
 				<p><strong>Pembukaan GSO VIII:</strong> 1 November 2014 </p>
 				Pertandingan akan dilaksanakan tanggal 1-7 November 2014 di SMAK GS.
-			</div>-->
+			</div>
 	

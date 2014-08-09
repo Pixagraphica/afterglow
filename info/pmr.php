106947880e71437caa45b0ca009e3a1a1f8bc807
NathanChrs <jonathan.christopher@outlook.com>
2014-08-09 09:23:04 +0700

    added pmr info

--- info/pmr.php
+++ info/pmr.php
@@ -1,24 +1,68 @@
 			
 			<div class="content-item content-main" >
-				<!--<h2 class="neon-<?php echo $colors["pmr"]; ?>">Peraturan Umum</h2>-->				
 				
 				<h2 class="neon-<?php echo $colors["pmr"]; ?>">Peraturan</h2>
-				<p>
-				<p>
-					<strong>Proposal Terpisah</strong> <br>
-				</p>
-			</div>
+				<ul>
+					<li>Setiap peserta wajib mendaftar ulang saat technical meeting dan mengambil nomor urut perlombaan.</li>
+					<li>Peserta wajib hadir 30 menit sebelum lomba dimulai.</li>
+					<li>Apabila setelah 3x panggilan peserta tidak hadir di arena perlombaan, maka akan didiskualifikasi demi kelancaran acara.</li>
+				</ul>
 				
-			<div class="content-item">
-				<h3 class="neon-<?php echo $colors["pmr"]; ?>">Contact Person</h3>
-				<strong>Wilson:</strong> 0813 1408 7511<br>
-				<strong>Angel:</strong> 0821 1180 0197 	
+				<strong>Peserta</strong> <br>
+				<ul>
+					<li>Tiap sekolah berhak mengirimkan maksimal 3 tim dengan tiap timnya terdiri dari 2 orang (peserta tidak boleh berlomba rangkap).</li>
+					<li>Kategori lomba ini adalah campuran mata terbuka.</li>
+					<li>Peserta adalah anggota PMR tingkat Madya atau Wira di sekolahnya.</li>
+				</ul>
+				
+				<strong>Perlengkapan</strong> <br>
+				<ul>
+					<li>Tiap tim memakai seragam lengkap PMR masing-masing sekolah.</li>
+					<li>Tiap tim wajib membawa tali tandu dan bambunya masing-masing, serta perlengkapan membuat tandu darurat (mitela dan selimut).</li>
+				</ul>
+				
+				<strong>Tata Tertib Peserta</strong> <br>
+				<ul>
+					<li>Setiap peserta wajib mendaftar ulang saat technical meeting dan mengambil nomor urut perlombaan.</li>
+					<li>Peserta wajib hadir 30 menit sebelum lomba dimulai.</li>
+					<li>Apabila setelah 3x panggilan peserta tidak hadir di arena perlombaan, maka akan didiskualifikasi demi kelancaran acara.</li>
+				</ul>
+				
+				<strong>Juri</strong> <br>
+				<ul>
+					<li>PMI Kabupaten Tangerang.</li>
+					<li>Pelatih/pembina PMR SMAK PENABUR Gading Serpong.</li>
+				</ul>
+				
+				<strong>Sistem Penilaian</strong> <br>
+				<ul>
+					<li>Menggunakan sistem grade.</li>
+					<li>Waktu maksimal: 6 menit.</li>
+					<li>Kriteria penilaian:
+						<ol>
+							<li>Kecepatan</li>
+							<li>Kekuatan</li>
+							<li>Kebenaran</li>
+							<li>Kerapihan</li>
+						</ol>
+					</li>
+				</ul>
+				
+				<strong>Pemenang</strong> <br>
+				<p>Akan diambil 3 juara untuk setiap kategorinya baik tingkat Madya maupun Wira dengan hadiah berupa piala dan uang pembinaan.</p>
+
 			</div>
 			
-			<!--<div class="content-item">
+			<div class="content-item">
+				<h3 class="neon-<?php echo $colors["pmr"]; ?>">Contact Person</h3>
+				<strong>Wilson:</strong> 081314087511 <br>
+				<strong>Angel:</strong> 082111800197	
+			</div>
+			
+			<div class="content-item">
 				<h3 class="neon-<?php echo $colors["pmr"]; ?>">Biaya Pendaftaran</h3>
-				<strong>Rp N/A,- per tim</strong> <br>
-				Uang Walk Out sebesar Rp N/A,-
+				<strong>Rp 150.000,- per tim</strong> <br>
+				Uang Walk Out sebesar Rp 50.000,-
 			</div>
 			
 			<div class="content-item">
@@ -26,4 +70,5 @@
 				<p><strong>Technical Meeting:</strong> 24 Oktober 2014 </p>
 				<p><strong>Pembukaan GSO VIII:</strong> 1 November 2014 </p>
 				Pertandingan akan dilaksanakan tanggal 1-7 November 2014 di SMAK GS.
-			</div> -->
+			</div>
+				
